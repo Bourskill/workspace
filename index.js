1884661b6e44b565676a68ca7c40ce0ba96e5a51
Richard Garcia <criyeak@gmail.com>
2023-03-22 04:17:51 +0000

    master

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-console.log("Hello CodeSandbox");
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('database', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'mysql',
+    // Aquí puedes agregar más opciones de configuración, si las necesitas
+});
+
+const User = sequelize.define('user', {
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    lastName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+sequelize.sync()
+    .then(() => {
+        console.log('Base de datos sincronizada');
+    });
